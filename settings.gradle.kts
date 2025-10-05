9826a59c1b57175029cfb2fc3d286b20fc995d23
cristmasbox <sonne.zucker@gmx.de>
2025-10-05 14:22:31 +0200

    support for jitpack publishing added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-        //maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://jitpack.io") }
         gradlePluginPortal()
     }
 }
